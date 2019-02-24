406848c704cdfc7be194ca6d86c4e8235493d24f
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-02-24 18:54:57 +0000

    simplify pipeline code

--- demos/pipeline.jl
+++ demos/pipeline.jl
@@ -1,24 +1,20 @@
 using JuliaDBMeta, TableWidgets, StatsPlots, Interact, Blink
 
-function pipeline(table; loader = nothing)
-    filtered_data = selectors(table)
+function pipeline(input_data)
+    filtered_data = selectors(input_data)
     edited_data = dataeditor(filtered_data)
     viewer = dataviewer(edited_data)
     tabs = OrderedDict(
-        :filename => loader,
         :filtered_data => filtered_data,
         :edited_data => edited_data,
         :viewer => viewer
     )
     tabulator(tabs)
 end
-function pipeline()
-    loader = filepicker()
-    ui = Observable{Any}(loader)
-    # initialize the widget as a filepicker, when the filepicker gets used, replace with the output of `myui` called with the loaded table
-    Interact.@map! ui pipeline(loadtable(&loader), loader = loader)
-    ui
-end
+
+fn = filepicker()
+tabs = Interact.@map isempty(&fn) ? nothing : pipeline(loadtable(&fn))
+ui = Widgets.div(fn, tabs)
 
 w = Window()
-body!(w, pipeline())
+body!(w, ui)
